1924c16929663a310748a019376533f7ebcdecc3
Rohitvpatil0810 <rohitvpatil0810@gmail.com>
2022-02-17 15:30:55 +0530

    Express App and DataBase Connection

--- server/app.js
+++ server/app.js
@@ -1,1 +1,20 @@
-console.log("hello, world!");
+const express = require("express");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+const app = express();
+
+dotenv.config({ path: "./config.env" });
+
+const dbURI = process.env.DATABASE;
+const port = 5000;
+
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    app.listen(port);
+    console.log("connected to db and listening at port 5000");
+  })
+  .catch((err) => console.log(err));
+app.listen(3000);
+
+app.get("/", (req, res) => res.send("server listening at 3000 port!"));
