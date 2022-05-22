d90a8743badb983b88bd7701fb61697e3ac293b3
Rohitvpatil0810 <rohitvpatil0810@gmail.com>
2022-05-22 11:12:43 +0530

    Version 1.1.0

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,7 @@
 const Admin = require("../models/admin");
-
+const Doctor = require("../models/doctor");
+const Patient = require("../models/patient");
+const jwt = require("jsonwebtoken");
 const maxAge = 3 * 24 * 60 * 60;
 const { createToken } = require("../utils/createToken");
 const handleError = (err) => {
@@ -32,6 +34,36 @@ module.exports.admin_login = async (req, res) => {
   } catch (err) {
     const errors = handleError(err);
     res.status(400).json({ errors });
+  }
+};
+
+module.exports.auth = async (req, res) => {
+  if (req.cookies.jwt) {
+    const token = req.cookies.jwt;
+    if (token) {
+      jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
+        if (err) {
+          res.status(100).json({ msg: "Proceed to login" });
+        } else {
+          const admin = await Admin.findById(decodedToken.id);
+          const patient = await Patient.findById(decodedToken.id);
+          const doctor = await Doctor.findById(decodedToken.id);
+          if (admin) {
+            res.status(200).json({ msg: "Admin Login Found" });
+          }
+          if (patient) {
+            res.status(200).json({ msg: "Patient Login Found" });
+          }
+          if (doctor) {
+            res.status(200).json({ msg: "Doctor Login Found" });
+          }
+        }
+      });
+    } else {
+      res.status(100).json({ msg: "Proceed to login" });
+    }
+  } else {
+    res.status(100).json({ msg: "Proceed to login" });
   }
 };
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { admin_login } = require("../controllers/authControllers");
+const { admin_login, auth } = require("../controllers/authControllers");
 
 const router = Router();
 
+router.get("/auth", auth);
 router.post("/login/admin", admin_login);
 
 module.exports = router;
