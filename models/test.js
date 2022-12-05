5fa4452f99f414a69c9ddb8b8b2f5367c9af0042
Rohit Patil <rohitvpatil0810@gmail.com>
2022-12-05 14:51:01 +0530

    Some no changes

--- models/test.js
+++ models/test.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt");
 async function genPassword(password) {
   const salt = await bcrypt.genSalt();
   password = await bcrypt.hash(password, salt);
+  console.log(password);
 }
-const password = "test1234";
+const password = "rohit@08";
 genPassword(password);
